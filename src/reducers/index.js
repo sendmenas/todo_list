9f6d6851a1a9dab0c68c9a9c61d83dcbfa7c0e85
Giedrius Bielskis <giedrius.bielskis@dukascopy.com>
2018-08-09 17:20:19 +0300

    Initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,45 @@
+import { combineReducers } from 'redux';
+import { VisibilityFilters, SET_VISIBILITY_FILTER, ADD_TODO, TOGGLE_TODO } from '../actions';
+
+const { SHOW_ALL } = VisibilityFilters;
+
+function visibilityFilter(state = SHOW_ALL, action) {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.filter;
+        default:
+            return state;
+    }
+}
+
+function todos(state = [], action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        case TOGGLE_TODO:
+            return state.map((todo) => {
+                if (todo.id === action.id) {
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    })
+                }
+                return todo;
+            });
+        default:
+            return state;
+    }
+}
+
+const todoApp = combineReducers({
+    visibilityFilter,
+    todos,
+});
+
+export default todoApp;
